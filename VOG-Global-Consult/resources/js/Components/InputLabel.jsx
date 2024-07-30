e955b920cced02f5f681aee9c24733de96353dd8
Nomso <94322130+heynomso@users.noreply.github.com>
2024-07-30 14:00:06 +0100

    Delete VOG-Global-Consult directory

--- VOG-Global-Consult/resources/js/Components/InputLabel.jsx
+++
@@ -1,7 +0,0 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (
-        <label {...props} className={`block font-montserrat font-semibold text-md md:text-2xl text-black ` + className}>
-            {value ? value : children}
-        </label>
-    );
-}
